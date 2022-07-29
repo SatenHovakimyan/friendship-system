d52780986faaa07fdfa048a60286c48228f9bd85
SatenHovakimyan <saten.hovakimyan5@gmail.com>
2022-07-29 13:49:34 +0400

    Initial commit

---
+++ src/main/java/com/saten/friendshipsystem/FriendshipSystemApplication.java
@@ -0,0 +1,13 @@
+package com.saten.friendshipsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendshipSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendshipSystemApplication.class, args);
+	}
+
+}
